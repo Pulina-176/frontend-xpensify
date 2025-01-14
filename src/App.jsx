df1636be48bac8e126285b3bbe324d83e20f5c13
W.H.P.T.H. Karunanayake <pulinathushan@gmail.com>
2025-01-14 12:20:38 +0530

    signin page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Routes>
     </div>
   );
